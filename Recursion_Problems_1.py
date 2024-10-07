5925bb26ae396e1bc5ae87c78f0901a8f25dcefd
Aarif-khan-mohammad <142022266+Aarif-khan-mohammad@users.noreply.github.com>
2024-10-07 17:14:34 +0530

    Recursion_Problems_1.py

--- Recursion_Problems_1.py
+++ Recursion_Problems_1.py
@@ -46,4 +46,12 @@ def first_n_even_natural_reverse(n):
 print()
 first_n_even_natural_reverse(10)
 
+# outputs
+# 1 2 3 4 5 6 7 8 9 10 
+# 10 9 8 7 6 5 4 3 2 1
+# 1 3 5 7 9 11 13 15 17 19
+# 2 4 6 8 10 12 14 16 18 20
+# 19 17 15 13 11 9 7 5 3 1
+# 20 18 16 14 12 10 8 6 4 2
+
 
